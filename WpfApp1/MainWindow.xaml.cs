310d2efe2843c9d07a3d12e73ffbac28016bd1ef
吳佳謙 <@LAPTOP-TUR32866>
2021-12-06 13:25:57 +0800

    ..

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,33 +20,54 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        double dtemp = 1;//宣告放大縮小時暫存變數
         public MainWindow()
         {
             InitializeComponent();
-           
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            media.Source = new Uri(@"E:\Wistron\VideoForTesting.mp4");
-            // 互動式控制
+            media.Source = new Uri(@"E:\Wistron\VideoForTesting.mp4");//宣告讀取路徑
+            //互動式控制
             media.LoadedBehavior = MediaState.Manual;
-            // 新增元素載入完成事件 -- 自動開始播放
+            //新增元素載入完成事件自動開始播放
             media.Loaded += new RoutedEventHandler(media_Loaded);
-            //// 新增媒體播放結束事件 -- 重新播放
-            //mediaElement.MediaEnded += new RoutedEventHandler(media_MediaEnded);
-            //// 新增元素解除安裝完成事件 -- 停止播放
-            //mediaElement.Unloaded += new RoutedEventHandler(media_Unloaded);
         }
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
-        {
-            
-        }
 
         private void media_Loaded(object sender, RoutedEventArgs e)
         {
-            (sender as MediaElement).Play();
+            (sender as MediaElement).Play();//執行播放
+        }
+
+        private void btnBig_Click(object sender, RoutedEventArgs e)//放大元素
+        {
+            if (dtemp <= 2.5)//設定最大值
+            {
+                dtemp += 0.1;//放大10%
+                media.RenderTransform = new ScaleTransform(dtemp, dtemp, 0.5, 0.5);//設定frame元素大小及中心點
+            }
+            else
+                MessageBox.Show("已達最大值!最大值:" + dtemp * 100 + "%");
+
+        }
+
+        private void btnsmall_Click(object sender, RoutedEventArgs e)//縮小元素
+        {
+            if (dtemp > 0.1)//設定最小值
+            {
+                media.RenderTransform = new ScaleTransform(dtemp - 0.1, dtemp - 0.1, 0.5, 0.5);//設定frame元素大小及中心點
+                dtemp -= 0.1;//縮小10%
+            }
+            else
+                MessageBox.Show("已達最小值!最小值:" + 0 * 100 + "%");
+                
+        }
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
     
